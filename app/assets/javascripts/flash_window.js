18bc741b550bcf8d62b9edab51a082cedc0c0b0e
Nakamura-Yasuhiro <mgmkd35845@gmail.com>
2020-12-01 12:33:10 +0900

    UI追加

--- app/assets/javascripts/flash_window.js
+++ app/assets/javascripts/flash_window.js
@@ -1,7 +1,7 @@
 addEventListener('load', ()=> {
     const flashWindow = document.getElementsByClassName("flash-window")[0];
     flashWindowToggle();
-    setTimeout(flashWindowToggle, 3000)
+    setTimeout(flashWindowToggle, 60000)
     document.getElementsByClassName("flash-window--delete")[0].addEventListener('click', flashWindowToggle);
     function flashWindowToggle() {
       flashWindow.classList.toggle("hidden");
