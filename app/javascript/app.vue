f8166a19a4a0d90ae43136617650384c0bb3df02
Nakamura-Yasuhiro <mgmkd35845@gmail.com>
2020-12-07 12:01:55 +0900

    footer追加

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,11 +1,5 @@
 <template>
   <v-app>
-    <v-alert
-      :value="true"
-      type="success"
-    >
-      This is a success alert.
-    </v-alert>
 
 
 <v-container class="mt-12">
@@ -14,19 +8,60 @@
   </v-row>
 </v-container>
 
+
+
+
+<template>
+  <v-footer
+    dark
+    padless
+  >
+    <v-card
+      flat
+      tile
+      class="indigo lighten-1 white--text text-center"
+    >
+      <v-card-text>
+        <v-btn
+          v-for="icon in icons"
+          :key="icon"
+          class="mx-4 white--text"
+          icon
+        >
+          <v-icon size="24px">
+            {{ icon }}
+          </v-icon>
+        </v-btn>
+      </v-card-text>
+
+      <v-card-text class="white--text pt-0">
+        Phasellus feugiat arcu sapien, et iaculis ipsum elementum sit amet. Mauris cursus commodo interdum. Praesent ut risus eget metus luctus accumsan id ultrices nunc. Sed at orci sed massa consectetur dignissim a sit amet dui. Duis commodo vitae velit et faucibus. Morbi vehicula lacinia malesuada. Nulla placerat augue vel ipsum ultrices, cursus iaculis dui sollicitudin. Vestibulum eu ipsum vel diam elementum tempor vel ut orci. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+      </v-card-text>
+
+      <v-divider></v-divider>
+
+      <v-card-text class="white--text">
+        {{ new Date().getFullYear() }} — <strong>Vuetify</strong>
+      </v-card-text>
+    </v-card>
+  </v-footer>
+</template>
   </v-app>
 </template>
 
 
 
 <script>
-export default {
-  data: function () {
-    return {
-      message: "Hello Vue!"
-    }
+  export default {
+    data: () => ({
+      icons: [
+        'mdi-facebook',
+        'mdi-twitter',
+        'mdi-linkedin',
+        'mdi-instagram',
+      ],
+    }),
   }
-}
 </script>
 
 <style scoped>
@@ -36,5 +71,14 @@ p {
 }
 .v-application .mt-12 {
     margin-top: 70vh!important;
+}
+.theme--light.v-card {
+    background-color: #fff;
+    margin-top: 34vw!important;
+    color: rgba(0,0,0,.87);
+}
+.v-sheet.v-footer:not(.v-sheet--outlined) {
+    box-shadow: 0 0 0 0 rgba(0,0,0,.2), 0 0 0 0 rgba(0,0,0,.14), 0 0 0 0 rgba(0,0,0,.12);
+    margin-top: 23rem;
 }
 </style>
