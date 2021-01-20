8cda8aa2bcd82f4c97e29fb9252b89fe6296c16b
Nakamura-Yasuhiro <mgmkd35845@gmail.com>
2021-01-20 12:42:40 +0900

    簡単なテスト

--- spec/system/enquetes_spec.rb
+++ spec/system/enquetes_spec.rb
@@ -5,32 +5,17 @@ RSpec.describe "FoodEnquetes", type: :system do
     context "アンケートに回答する" do
       it "回答できること" do
         enquete = FactoryBot.build(:food_enquete_tanaka)
-        visit "/top/index"
-        fill_in 'お名前', with: enquete.name
-        fill_in 'メールアドレス', with: enquete.mail
-        fill_in '年齢', with: enquete.age
-        select enquete.food_name, from: 'お召し上がりになった料理'
-        choose "food_enquete_score_#{enquete.score}"
-        fill_in 'ご意見・ご要望', with: enquete.request
-        select enquete.present_name, from: 'ご希望のプレゼント'
+        visit "/home/index"
+        visit "/guest_login"
+        visit "/posts/index"
 
-        # 【補足】見た目をわかりやすくするために動作を2秒止めています。
-        # 通常は自動テストが遅くなりますので不要な処理です。
+        fill_in "content", with: "波乗りジョニー"
         sleep 2
-        click_button '登録する'
+        click_on '投稿'
 
-        expect(page).to have_content 'ご回答ありがとうございました'
-        expect(page).to have_content 'お名前: 田中 太郎'
-        expect(page).to have_content 'メールアドレス: taro.tanaka@example.com'
-        expect(page).to have_content '年齢: 25'
-        expect(page).to have_content 'お召し上がりになった料理: やきそば'
-        expect(page).to have_content '満足度: 良い'
-        expect(page).to have_content 'ご意見・ご要望: おいしかったです。'
-        expect(page).to have_content 'ご希望のプレゼント: 【10名に当たる】ビール飲み放題'
+        sleep 2
 
-        # 【補足】見た目をわかりやすくするために動作を5秒止めています。
-        # 通常は自動テストが遅くなりますので不要な処理です。
-        sleep 5
+
       end
     end
   end
@@ -38,22 +23,15 @@ RSpec.describe "FoodEnquetes", type: :system do
   describe '異常' do
     context "必須項目が未入力" do
       it "エラーメッセージが表示され、回答できないこと" do
-        visit "/top/index"
-
-        # 【補足】見た目をわかりやすくするために動作を2秒止めています。
-        # 通常は自動テストが遅くなりますので不要な処理です。
+        visit "/home/index"
+        visit "/guest_login"
+        visit "/posts/index"
+        fill_in "content", with: "波乗りジョニー"
         sleep 2
-        click_button '登録する'
+        click_on '投稿'
+        sleep 2
 
-        expect(page).not_to have_content 'ご回答ありがとうございました'
-        expect(page).to have_content 'お名前を入力してください'
-        expect(page).to have_content 'メールアドレスを入力してください'
-        expect(page).to have_content '年齢を入力してください'
-        expect(page).to have_content '満足度を入力してください'
 
-        # 【補足】見た目をわかりやすくするために動作を5秒止めています。
-        # 通常は自動テストが遅くなりますので不要な処理です。
-        sleep 5
       end
     end
   end
